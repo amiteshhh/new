4d8340abbb0bd62bbec5ef1028b7e22fe21a916b
Amitesh Kumar <amiteshhh,nitk@gmail.com>
2017-01-14 00:52:57 +0530

    event modal

--- api/models/Event.js
+++ api/models/Event.js
@@ -1,25 +1,18 @@
 /**
- * User.js
+ * Event.js
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
 module.exports = {
+
   attributes: {
-    username: {
+    title: {
       type: 'string'
     },
-    email: {
+    description: {
       type: 'string'
-    },
-    mobile: {
-      type: 'string',
-      unique: true
-    },
-    password: {
-      type: 'string',
-      unique: true
     }
   }
 };
