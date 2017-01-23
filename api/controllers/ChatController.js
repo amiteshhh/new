9b21c7bfe47b9876e4923d3638e297e5419ac9cc
Amitesh Kumar <amiteshhh,nitk@gmail.com>
2017-01-23 13:52:20 +0530

    chat

--- api/controllers/ChatController.js
+++ api/controllers/ChatController.js
@@ -7,11 +7,11 @@ module.exports = {
     // Use that ID to look up the user in the session
     // We need to do this because we can have more than one user
     // per session, since we're creating one user per socket
-    //OnlineUser.findOne(req.session.users[socketId]).exec(function(err, sender) {
+    //OnlineUser.findOne(req.session.users[socketId].id).exec(function(err, sender) {
       // Publish a message to that user's "room".  In our app, the only subscriber to that
       // room will be the socket that the user is on (subscription occurs in the onConnect
       // method of config/sockets.js), so only they will get this message.
-      console.log('from', req.param('from'))
+      console.log('private msg from', req.param('from'));
       OnlineUser.message(req.param('to'), {
         from: req.param('from'),
         msg: req.param('msg')
@@ -24,24 +24,25 @@ module.exports = {
   // Post a message in a public chat room
   public: function(req, res) {
     // Get the ID of the currently connected socket
-    var socketId = sails.sockets.getId(req);
+    //var socketId = sails.sockets.getId(req);
     // Use that ID to look up the user in the session
     // We need to do this because we can have more than one user
     // per session, since we're creating one user per socket
-    OnlineUser.findOne(req.session.users[socketId].id).exec(function(err, user) {
+    //OnlineUser.findOne(req.session.users[socketId].id).exec(function(err, user) {
       // Publish a message to the room's "room".  Every user in the room will have their socket
       // subscribed to it, so they'll all get the message.  The user who created the room gets
       // their socket subscribed to it in RoomController.create; everyone who joins later gets
       // subscribed in RoomController.join.
+      console.log('private msg from', req.param('from'));
       Room.message(req.param('room'), {
         room: {
           id: req.param('room')
         },
-        from: user,
+        from: req.param('from'),
         msg: req.param('msg')
       }, req.socket);
 
-    });
+    //});
 
   }
 
